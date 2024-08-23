af7cbbe935a2c76760a7eacf4788cae9a5c980cb
ielen97.97@gmail.com <ielen97.97@gmail.com>
2024-08-23 14:12:21 +0300

    init commit

---
+++ Lesson3_2.py
@@ -0,0 +1,17 @@
+# creating your "awesome" list
+# lst = [1, 2, 3, 4, 5, 6]
+# lst = [12, 3, 4, 10]
+# lst = [1]
+# lst = []
+lst = [12, 3, 4, 10, 8]
+
+# if the list is blank
+if len(lst) == 0:
+    result = [] # if the list is blank the result is also blank
+elif len(lst) == 1:
+    result = lst # if the list contains one element it remains the same
+else:
+    last_element = lst[-1]
+    result = [last_element] + lst[:-1] # a new list, where the last element becomes the first
+
+print(result)
