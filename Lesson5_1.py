a72807ac5c1e49b4f89c17a931cb50bbb8d0dcb0
ielen97.97@gmail.com <ielen97.97@gmail.com>
2024-09-01 14:19:50 +0300

    init commit

---
+++ Lesson5_1.py
@@ -0,0 +1,37 @@
+# let's check if given string can be the name of a variable
+# access the list of all reserved keywords
+import keyword
+
+# test list
+test_data = ["_", "__", "___", "x", "get_value", "get value", "get!value", "some_super_puper_value",
+             "Get_value", "get_Value", "getValue", "3m", "m3", "assert", "assert_exception"]
+
+# iterates through the list
+for test_var in test_data:
+    # gets an input from a user:
+    # test_var = input("Enter a variable name: ")
+
+    # checks if the variable name is not empty
+    if not test_var:
+        is_valid = False
+    else:
+    # if the name consists of a single character, it must be an underscore or a letter
+        if len(test_var) == 1:
+            if not (test_var.isalpha() or test_var == "_"):
+                is_valid = False
+            else:
+                is_valid = True
+        else:
+        # if the name consists of multiple characters it must start with a letter
+            if not test_var[0].isalpha():
+                is_valid = False
+            else:
+            # must be entirely lowercase, and contain only letters, numbers, and underscores
+                if not (test_var.islower() and test_var.replace("_", "").isalnum()):
+                    is_valid = False
+                else:
+                    is_valid = True
+        # checks if it's NOT a reserved keyword
+        if keyword.iskeyword(test_var):
+            is_valid = False
+    print(is_valid)
