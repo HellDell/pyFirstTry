f82e590d98a19cd4d1196e45fca56bcc62a46987
ielen97.97@gmail.com <ielen97.97@gmail.com>
2024-09-05 14:46:45 +0300

    init commit

--- Lesson7_1.py
+++ Lesson7_1.py
@@ -1,8 +1,20 @@
-
+# creating a function that will represent a person according to the passed parameters
 def say_hi(name: str, age: int):
-    print(f"Hi, my name is {name} and I'm {age} years old")
 
+    #v1 w/ print (won't pass unit tests)
+    # print(f"Hi. My name is {name} and I'm {age} years old")
+
+    # v2 w/ return (will pass unit tests)
+    return f"Hi. My name is {name} and I'm {age} years old"
+
+# v1 with pre-defined arguments
+# say_hi(name="Alex", age=32)
+
+# v2 asks user for an input and returns a string (will pass unit tests)
 name = input("What's your name? ")
 age = int(input("How old are you? "))
 
-say_hi(name, age)
+#unit tests
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
