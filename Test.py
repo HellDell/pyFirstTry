a291e819cb631e3126054858ff5ce8a5f1b0849d
Olena aka Lena <ielen97.97@gmail.com>
2024-09-12 13:25:44 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 # a func that calculates the rectangle area
-
+#v1
 def area_of_rectangle(a, b):
   area = a * b
   print("The area of a rectangle with sides", a, "and", b, "is equal to:", area)
@@ -7,3 +7,12 @@ def area_of_rectangle(a, b):
 length = 5
 width = 3
 area_of_rectangle(length, width)
+
+# simple version goes here
+# v2
+
+a = 5
+b = 10
+area_of_rectangle = a * b
+
+print(area_of_rectangle)
