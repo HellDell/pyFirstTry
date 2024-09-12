e0667f96c25204778c2cbab39e1d524e3150d328
ielen97.97@gmail.com <ielen97.97@gmail.com>
2024-09-12 13:22:23 +0300

    init commit

---
+++ Test.py
@@ -0,0 +1,9 @@
+# a func that calculates the rectangle area
+
+def area_of_rectangle(a, b):
+  area = a * b
+  print("The area of a rectangle with sides", a, "and", b, "is equal to:", area)
+
+length = 5
+width = 3
+area_of_rectangle(length, width)
