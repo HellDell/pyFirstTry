94df1bb38cf5e5a1a401ae0dd4e0a9d7a3684e7b
ielen97.97@gmail.com <ielen97.97@gmail.com>
2024-10-04 14:12:10 +0300

    init commit

---
+++ 14_2/Group.py
@@ -0,0 +1,42 @@
+class GroupLimitError(Exception):
+
+    def __init__(self, message="A group cannot contain more than 10 students!"):
+        self.message = message
+        super().__init__(self.message)
+
+class Group:
+
+    MAX_STUDENTS = 10  # max student num in a group
+
+    def __init__(self, number):
+        self.number = number
+        self.group = []
+
+    def add_student(self, student):
+        if len(self.group) >= Group.MAX_STUDENTS:
+            raise GroupLimitError()  # raise an exception
+        if student not in self.group:  # check for re-addition here
+            self.group.append(student)
+        else:
+            print(f"Student {student.last_name} is already in the group")
+
+    def __str__(self):
+        students_str = ""
+        for student in self.group:
+            students_str += str(student) + "\n"
+        return f"Number: {self.number}\n{students_str}"
+
+    def delete_student(self, last_name):
+        student_remove = self.find_student(last_name)
+        if student_remove:
+            self.group.remove(student_remove)
+
+            print(f"Student {student_remove.last_name} deleted")
+        else:
+            print(f"Student with last name {last_name} not found")
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
---
+++ 14_2/Student.py
@@ -0,0 +1,21 @@
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'First name: {self.first_name}, last name: ' \
+               f'{self.last_name}, age: {self.age}, gender: {self.gender} '
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'First name: {self.first_name}, last name: {self.last_name}, ' \
+               f'id student: {self.record_book}'
