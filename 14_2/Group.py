59ce2dbdded08178d7a9fa6819eb7cf6d8ee3514
ielen97.97@gmail.com <ielen97.97@gmail.com>
2024-10-04 14:19:42 +0300

    init commit

--- 14_2/Group.py
+++ 14_2/Group.py
@@ -1,8 +1,4 @@
-class GroupLimitError(Exception):
-
-    def __init__(self, message="A group cannot contain more than 10 students!"):
-        self.message = message
-        super().__init__(self.message)
+from Raised_exeption import GroupLimitError
 
 class Group:
 
---
+++ 14_2/Human.py
@@ -0,0 +1,11 @@
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'First name: {self.first_name}, last name: ' \
+               f'{self.last_name}, age: {self.age}, gender: {self.gender} '
